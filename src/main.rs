fbc470811b17c6598659dfe9d6809e62d46e85a5
Kirill Kozlov <kozlov.kirill.v.work@gmail.com>
2024-07-25 15:14:44 +0600

    Implement processing audion from multipart form-data

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,50 @@
-use axum::{extract, response::Html, routing, Router};
+use std::{fs::{File, remove_file}, io::Write};
+
+use axum::{
+    extract, 
+    http::StatusCode, 
+    routing, 
+    Router
+};
 use serde::{Deserialize, Serialize};
 
+use transcribot_whisper_wrapper::WhisperArgs;
+
 #[derive(Serialize, Deserialize)]
-struct UserContent {
-    id: u32,
-    content: String,
+struct RequestQuery {
+    id: u32
 }
 
-async fn handle_audio(
-    extract::Json(data): extract::Json<UserContent>
-) -> Html<String> {
-    Html(format!("Content {} from user {}", data.content, data.id))
+async fn upload(requset_query: extract::Query<RequestQuery>, mut multipart: extract::Multipart) -> Result<String, (StatusCode, String)> {
+    while let Some(field) = multipart
+        .next_field()
+        .await
+        .map_err(|err| (StatusCode::BAD_REQUEST, err.to_string()))?
+    {
+        if field.name().expect("Field name doesn't found").contains("file") {
+            if let Ok(bytes) = field.bytes().await {
+                let file_name = format!("./{}", requset_query.id);
+                let mut audio_file = File::create_new(file_name.clone()).map_err(|_| (
+                    StatusCode::TOO_MANY_REQUESTS, String::from("Please wait till previous file will be processed")
+                ))?;
+                audio_file.write(&bytes).expect("Failed to write data");
+                println!("File saved");
+                let w_args = WhisperArgs::new(
+                    String::from("ru"), file_name.clone()
+                );
+                let res = w_args.run_whisper().map_err(|err| (StatusCode::BAD_REQUEST, err.to_string()))?;
+                remove_file(file_name).map_err(|err| (StatusCode::BAD_REQUEST, err.to_string()))?;
+                return Ok(res);
+            }
+        }
+    }
+
+    Err((StatusCode::BAD_REQUEST, String::from("File wasn't recieved")))
 }
 
 fn set_router() -> Router {
     Router::new()
-        .route("/", routing::post(handle_audio))
+        .route("/", routing::post(upload))
 }
 
 #[tokio::main]
